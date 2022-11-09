f7769007568835d539c89e1efcad261602890b6e
Надія Ткач <nadiya77723@gmail.com>
2022-11-09 13:36:40 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
+
+
 import React from 'react';
-import axios from 'axios';
 // import { ToastContainer } from 'react-toastify';
 import Searchbar from './Searchbar';
 // import ImageGallery from './ImageGallery';
@@ -7,28 +8,26 @@ import Searchbar from './Searchbar';
 // import Loader from './Loader';
 // import Button from './Button';
 // import Modal from './Modal';
-// import fetchPicture from 'services/api';
-
-axios.defaults.baseURL = 'https//pixabay.com/api/';
-const API_KEY = '30077711-4b113b89ab0e54a97a0c4d035';
+import fetchPicture from 'services/api';
 
 export class App extends React.Component {
   state = {
     pictures: [],
-    // query: '',
-    // page: 1,
-    // perPage: 12,
-    // showModal: false,
-    // status: 'idle',
+    query: '',
+    page: 1,
+    perPage: 12,
+    showModal: false,
+    status: 'idle',
   };
 
-  // componentDidUpdate() {}
+  // componentDidUpdate(prevProps, prevState) {}
 
   handleQuerySubmit = query => {
     this.setState({ query });
   };
 
   render() {
+    const { pictures, error, status } = this.state;
     return (
       <>
         <Searchbar handleQuerySubmit={this.handleQuerySubmit} />
@@ -37,50 +36,8 @@ export class App extends React.Component {
         <Loader />
         <Button />
         <Modal /> */}
-        {/* <ToastContainer autoClose={3000} /> */}
+        <ToastContainer autoClose={3000} />
       </>
     );
   }
 }
-
-// import React from 'react';
-// // import { ToastContainer } from 'react-toastify';
-// import Searchbar from './Searchbar';
-// // import ImageGallery from './ImageGallery';
-// // import ImageGalleryItem from './ImageGalleryItem';
-// // import Loader from './Loader';
-// // import Button from './Button';
-// // import Modal from './Modal';
-// import fetchPicture from 'services/api';
-
-// export class App extends React.Component {
-//   state = {
-//     pictures: [],
-//     query: '',
-//     page: 1,
-//     perPage: 12,
-//     showModal: false,
-//     status: 'idle',
-//   };
-
-//   // componentDidUpdate(prevProps, prevState) {}
-
-//   handleQuerySubmit = query => {
-//     this.setState({ query });
-//   };
-
-//   render() {
-//     const { pictures, error, status } = this.state;
-//     return (
-//       <>
-//         <Searchbar handleQuerySubmit={this.handleQuerySubmit} />
-//         {/* <ImageGallery />
-//         <ImageGalleryItem />
-//         <Loader />
-//         <Button />
-//         <Modal /> */}
-//         <ToastContainer autoClose={3000} />
-//       </>
-//     );
-//   }
-// }
